c61ace43098ca4d9a151cfed18f39f0aad116a8b
ajaykk1 <73517233+ajaykk1@users.noreply.github.com>
2021-12-26 01:31:32 +0530

    initial commit

---
+++ src/Helicopter.java
@@ -0,0 +1,34 @@
+public class Helicopter {
+    public static void main(String[]args) throws InterruptedException {
+        while (true) {
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println("               +========");
+            System.out.println("              | |");
+            System.out.println("         //-------------");
+            System.out.println("=========             ()\\");
+            System.out.println("         \\               \\");
+            System.out.println("          \\                ]");
+            System.out.println("            ----------------");
+            System.out.println("               I         I    ");
+            System.out.println("            -------------------");
+            Thread.sleep(100);
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println("       ========+");
+            System.out.println("              | |");
+            System.out.println("         //-------------");
+            System.out.println("=========             ()\\");
+            System.out.println("         \\               \\");
+            System.out.println("          \\                ]");
+            System.out.println("            ----------------");
+            System.out.println("               I         I    ");
+            System.out.println("            -------------------");
+            Thread.sleep(100);
+        }
+    }
+}
